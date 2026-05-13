cb08d1da364d57fabe9474a43e01e34216ba04b2
05parmar <rp913018@gmail.com>
2026-05-13 18:50:15 +0530

    patch 4

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const SizedBox(width: 16),
               ElevatedButton(
                 onPressed: _changeText,
-                child: const Text('change'),
+                child: const Text('change text'),
               ),
             ],
           ),
